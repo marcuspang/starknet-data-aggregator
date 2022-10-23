7d66414f429f433d1a0de55a7186aa978a03cd53
Ryan Martin <rmrt1n@users.noreply.github.com>
2022-10-23 18:53:15 +0800

    feat: create landing & search result page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import Header from '../components/Header'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Starknet Data Aggregator</title>
+        <meta name="description" content="Starknet Data Aggregator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="bg-neutral-900 text-neutral-100 text-lg md:text-xl">
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fb2576',
+        secondary: '#231f3f',
+      }
+    },
   },
   plugins: [],
-};
+}
